e1f998321f8e2cdf79a74e40fb6820c8c4829d01
ORION <73669069+bobsub218@users.noreply.github.com>
2021-07-31 22:04:27 +0200

    Update contract_read.py

--- Python-Matic-SDK/examples/contract_read.py
+++ Python-Matic-SDK/examples/contract_read.py
@@ -4,6 +4,7 @@ evc = EVCore(verbose=False)
 # put in a contract address deployed from your MaticVigil account
 contract_instance = evc.generate_contract_sdk(
     contract_address='0xContractAddress',
-    app_name='microblog')
+    app_name='microblog'
+)
 # calling the blogTitle() function on the contract. This is a 'read' call, does not change state of the contract
 print(contract_instance.blogTitle())
